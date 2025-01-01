a1a46ddc1ba9a1fccc67e3644ea1c17397014520
Joshua <amethystguitaristj@gmail.com>
2025-01-01 21:08:04 +0000

    initialization and installation

---
+++ src/app/services/chat/chat.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { SocketService } from "./socketioImp.service";
+
+@Injectable({providedIn: 'root'})
+export class ServiceNameService {
+    constructor() { }
+    
+}
