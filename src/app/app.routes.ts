d3ee3f9a954438405197f3fcdf6d54ccf43ad92c
Joshua <amethystguitaristj@gmail.com>
2025-01-02 22:30:27 +0000

    debug: changed location for signup and chat components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from "./ui/auth/signup/signup.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"create",component:SignupComponent,title:"Create Account"}
+];
