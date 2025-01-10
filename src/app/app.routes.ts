fb85acafbd51b92ff7e12225a64177daf42f8d80
DevHeris <badairoinioluwa578@gmail.com>
2025-01-10 11:47:28 +0100

    CART PAGE COMPLETION

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { SignupComponent } from './ui/auth/signup/signup.component';
 import { OrderComponent } from './pages/order/order.component';
 import { MarketplaceComponent } from './pages/marketplace/marketplace.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'marketplace', pathMatch: 'full' },
@@ -18,4 +19,5 @@ export const routes: Routes = [
     title: 'Markt | Create account',
   },
   { path: 'order', component: OrderComponent, title: 'Markt | Your orders' },
+  { path: 'cart', component: CartComponent, title: 'Markt | Your cart' },
 ];
