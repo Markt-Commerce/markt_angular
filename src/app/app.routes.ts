c82f80737b78499da0518cb440f35e173eaada1c
seniorcub <farinderif@gmail.com>
2025-01-14 18:19:43 +0100

    Seller Order Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { SignupComponent } from "./ui/auth/signup/signup.component";
 import { ErrorComponent } from './ui/error/error.component';
 import { SellerProfileComponent } from './ui/seller-profile/seller-profile.component';
 import { SellerDashboardComponent } from './ui/seller-dashboard/seller-dashboard.component';
+import { SellerOrdersComponent } from './ui/seller-orders/seller-orders.component';
 
 export const routes: Routes = [
   { path: "signup", component: SignupComponent },
   { path: 'error', component: ErrorComponent },
   { path: 'seller/profile', component: SellerProfileComponent},
   { path: "seller/dashboard", component: SellerDashboardComponent },
+  { path: "seller/orders", component: SellerOrdersComponent },
   { path: '**', redirectTo: 'error' },
 ];
---
+++ src/app/ui/seller-orders/seller-orders.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { SideBarComponent } from '../side-bar/side-bar.component';
+import { FooterComponent } from '../footer/footer.component';
+import { EachSellerOrderComponent } from '../each-seller-order/each-seller-order.component';
+
+@Component({
+  selector: 'app-seller-orders',
+  standalone: true,
+  imports: [ SideBarComponent, FooterComponent, EachSellerOrderComponent ],
+  templateUrl: './seller-orders.component.html',
+  styleUrl: './seller-orders.component.css'
+})
+export class SellerOrdersComponent {
+
+}
